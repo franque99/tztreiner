9ff6e3652cf83c3e356f1191ed80e2bbe033710a
franque99 <mastergto199@gmail.com>
2025-07-07 02:48:55 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Lógica simples temporária para mudar a mão futuramente
+document.querySelector(".next-hand").addEventListener("click", () => {
+  alert("Próxima mão ainda não implementada!");
+});
